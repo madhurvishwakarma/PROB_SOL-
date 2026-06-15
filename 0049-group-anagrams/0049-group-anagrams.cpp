cd52d622adc7087ea29f771da36161e3b0adb613
Levi <b24ee1041@iitj.ac.in>
2026-06-15 23:03:21 +0530

    Time: 79 ms (5.63%), Space: 52.1 MB (5.3%) - LeetHub - Time: O(NM), Space: O(NM)

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map< map<char , int> , vector<string> > mp ;
+        int n = strs.size() ;
+        for( int i = 0 ; i<n ; i++ ){
+            map< char , int > np ;
+            for( auto x : strs[i] ){
+                np[x]++ ;
+            }
+            mp[np].push_back(strs[i]) ;
+        }
+        vector<vector<string>> v;
+        for( auto x  : mp ){
+            v.push_back( x.second ) ;
+        }
+        return v ;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
