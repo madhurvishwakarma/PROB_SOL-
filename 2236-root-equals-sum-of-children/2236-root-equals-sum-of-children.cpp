3ea023e9901f241fa2e362837dd791415c4abdae
Levi <b24ee1041@iitj.ac.in>
2026-07-05 14:31:39 +0530

    Time: 0 ms (100%), Space: 15.3 MB (3.91%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if( root == NULL ){
+            return false ;
+        }
+        if( root->val == root->right->val + root->left->val ){
+            return true ;
+        }
+        return false ;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
