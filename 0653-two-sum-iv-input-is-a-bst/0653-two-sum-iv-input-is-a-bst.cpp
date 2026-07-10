76d9da10aedadd0f0eaa451e6c817c3c7337b606
Levi <b24ee1041@iitj.ac.in>
2026-07-10 15:14:53 +0530

    Time: 3 ms (52.45%), Space: 37.4 MB (46.92%) - LeetHub - Time: O(n), Space: O(n)

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,26 +1,57 @@
+class BST{
+private: 
+   stack<TreeNode*> st ;
+   bool reverse = false ;
+   void pushAll( TreeNode* root ){
+    while( root ){
+        st.push(root) ;
+        if( reverse == false ){
+            root = root->left ;
+        }
+        else{
+            root = root->right ;
+        }
+    }
+   }
+public: 
+   BST(TreeNode* root , bool isreverse){
+    reverse = isreverse ;
+    pushAll(root) ;
+   }
+   bool HasNxt(){
+    return !st.empty() ;
+   }
+   int next(){
+    TreeNode* node = st.top() ;
+    st.pop() ;
+    if(reverse == false ){
+        pushAll( node->right ) ;
+    }
+    else{
+        pushAll( node->left ) ;
+    }
+    return node->val ;
+   }
+};
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& v) {
-        if(root == NULL) return;
-        inorder(root->left, v);
-        v.push_back(root->val);
-        inorder(root->right, v);
-    }
-
     bool findTarget(TreeNode* root, int k) {
-        vector<int> v;
-        inorder(root, v);  
-
-        int l = 0, r = v.size() - 1;
-        while(l < r) {
-            int sum = v[l] + v[r];
-            if(sum == k) return true;
-            else if(sum < k) l++;  
-            else r--;               
+        if( root == NULL ){
+            return false ;
         }
-        return false;
+        BST l(root , false) ;
+        BST r(root , true ) ;
+        int left = l.next() ;
+        int right = r.next() ;
+        while( left < right ){
+            if( left + right == k ) return true ;
+            else if( left + right < k ) left = l.next() ;
+            else right = r.next() ;
+        }
+        return false ;
     }
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
