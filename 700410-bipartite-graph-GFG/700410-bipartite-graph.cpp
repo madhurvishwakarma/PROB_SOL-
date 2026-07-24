8e52d8e53bb366d7174be7d5d1a48dfb10197023
Levi <b24ee1041@iitj.ac.in>
2026-07-24 16:32:31 +0530

    Added solution

--- 700410-bipartite-graph-GFG/700410-bipartite-graph.cpp
+++ 700410-bipartite-graph-GFG/700410-bipartite-graph.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-      bool isBipartite(int V, vector<vector<int>> &list) {
+    bool isBipartite(int V, vector<vector<int>> &list) {
         vector<vector<int>> edges( V ) ;
         for( int i=0 ; i<list.size() ; i++ ){
             edges[list[i][0]].push_back(list[i][1]) ;
@@ -9,28 +9,24 @@ class Solution {
         vector<int> color( V , -1 ) ;  
         for( int i =0 ; i< V ; i++ ){
             if( color[i] == -1 ){
-                if( check( i , edges , color ) == false ){
+                if( check( i , 0 ,  edges , color ) == false ){
                     return false ;
                 }
             }
         }      
         return true ;
     }   
-    bool check( int node , vector<vector<int>>& edges , vector<int>& color ){
-        color[node] = 0 ;
-        queue<int> q ;
-        q.push(node) ;
-        while( !q.empty() ){
-            int n = q.front() ;
-            q.pop() ;
-            for( auto x : edges[n]){
-                if( color[x] == -1 ){
-                    color[x] = !color[n] ;
-                    q.push(x) ;
-                }
-                else if( color[x] == color[n]){
+    bool check( int node , int c , vector<vector<int>>& edges , vector<int>& color ){
+        color[node] = c ;
+        for( auto x : edges[node]){
+            if( color[x] == -1 ){
+                color[x] = !color[node] ;
+                if( check( x , color[x] , edges , color ) == false ){
                     return false ;
                 }
+            }
+            else if( color[x] == color[node]){
+                return false ;
             }
         }
         return true ;
