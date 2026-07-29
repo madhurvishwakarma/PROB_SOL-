7add568cc6af305f941598ffc9198e65b123a4e9
Levi <b24ee1041@iitj.ac.in>
2026-07-29 11:33:26 +0530

    Time: 0 ms (100%), Space: 10.9 MB (19.1%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int calculate( vector<int>& nums , int start , int end ){
-        int n = end - start +1 ;
+    int calculate( vector<int>& nums ){
+        int n = nums.size() ;
         if( n < 0 ) return 0 ;
-        if( n == 0 ) return nums[0] ;
-        if( n == 1 ) return nums[start] ;
-        vector<int> dp( n  ,-1 ) ;
-        dp[0] = nums[start] ;
-        for( int i=1 ; i<n ; i++ ){
-            int take = nums[i+start] ;
-            if( i > 1) take += dp[(i-2)%n] ;
+        vector<int> dp ( n , -1 ) ;
+        dp[0] = nums[0] ;
+        for( int i=1  ; i<n ; i++ ){
+            int take = nums[i] ;
+            if( i > 1 ) take += dp[i-2] ;
             int Ntake = dp[i-1] ;
             dp[i] = max( take , Ntake ) ;
         }
-        int sum = dp[n-1] ;
-        return sum ;
+        return dp[n-1] ;
     }
     int rob(vector<int>& nums) {
         int n = nums.size() ;
-        vector<int> dp( n  ,-1 ) ;
-        return max( calculate(nums , 0, n-2 ) , calculate(nums,1,n-1) ) ;
+        if( n == 1 ) return nums[0] ;
+        vector<int> v1 , v2 ;
+        for( int i=0 ; i<n ; i++ ){
+            if( i != 0 ) v1.push_back(nums[i]) ;
+            if( i != n-1 ) v2.push_back( nums[i]) ;
+        }
+        return max( calculate(v1) , calculate(v2)) ;
     }
 };
 
